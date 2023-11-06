dee2ab59b023d08050046fb2879cc80103aa3c4d
leafsphere <78115182+leafsphere@users.noreply.github.com>
2023-11-05 21:51:17 -0500

    Modified functionality and added global variable

--- R/plot.R
+++ R/plot.R
@@ -18,3 +18,12 @@ plot_accel = function(x, x_var = 'time') {
     geom_line() +
     facet_grid(Channel ~ .)
 }
+
+plot.spec_sig = function(x, y, ...) {
+  x |>
+    pivot_longer(-freq, values_to = "Accerleration", names_to = "Channel") |>
+    # ggplot(aes_string(x=x_Var, y="Acceleration")) +
+    ggplot(aes(x=freq, y=Acceleration)) +
+    geom_line() +
+    facet_grid(Channel ~ .)
+}
